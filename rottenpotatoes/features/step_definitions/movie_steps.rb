7f72fe585e046761bedf95d9f1c4409f37f46ef8
Malcolm Mashig <mjm2396@columbia.edu>
2021-10-17 03:29:26 +0000

    part 2 try 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-      Movie.create(movie)
+    Movie.create(movie)
   end
 end
 
@@ -28,11 +28,23 @@ end
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb"
+  split_ratings = rating_list.split(', ')
+  if not uncheck
+      split_ratings.each do |rating|
+          step %{I check "ratings_#{rating}"}
+      end
+  else 
+      split_ratings.each do |rating|
+          step %{I uncheck "ratings_#{rating}"}
+      end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  all_movies = Movie.all
+  all_movies.each do |movie| 
+      step %{I should see "#{movie.title}"}
+  end
 end
